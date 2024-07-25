3f9160070356bde41beac8fbc78079941810db7c
Youcheng <youcheng.416@gmail.com>
2024-07-25 16:07:34 -0700

    server file test

--- PBD_peel/bandage debug/tissue2tissue_test.py
+++ PBD_peel/bandage debug/tissue2tissue_test.py
@@ -16,19 +16,16 @@ from torchviz import make_dot
 # %%
 N_skin = 400
 N_bandage = 400
-control_trajectory = np.array([[-0.4677,  0.0367,  0.1300],
-                               [-0.4,  0.0367,  0.2300],
-                               [-0.3,  0.0367,  0.33],
-                               [-0.2,  0.0367,  0.43],
-                               [-0.0,  0.0367,  0.53],
-                               [0.2,  0.0367,  0.63],
-                               [0.4,  0.0367,  0.73],
-                               [0.6,  0.0367,  0.83]]) / 100
+control_trajectory = np.array([[0.0099, 0.0014, 0.0013],
+                               [0.0079, 0.0014, 0.0033],
+                               [0.0059, 0.0014, 0.0053],
+                               [0.0039, 0.0014, 0.0057],
+                               [0.0019, 0.0014, 0.0063],
+                               [-0.0039, 0.0014, 0.0063]])
 # control_trajectory = np.array([[-0.4677,  0.0367,  0.1300],
 #                                [-0.4677,  0.0367,  0.40],
 #                                [-0.4677,  0.0367,  0.60],
 #                                [-0.4677,  0.0367,  0.99]])
-control_trajectory[:, 2] += 0.0024
 
 # %%
 # interpolate trajectory
@@ -41,20 +38,15 @@ control_trajectory = torch.from_numpy(control_trajectory).to(cfg.device)
 # %%
 # load data
 softbody = XPBDSoftbody()
-skin_mesh = softbody.add_thinshell(pv.Cube(center=(0, 0, 0), x_length=0.01, y_length=0.008, z_length=0.0024), n_surf=N_skin)
-bandage_mesh = softbody.add_thinshell(pv.Cube(center=(0, 0, 0.0024), x_length=0.01, y_length=0.008, z_length=0.0024), n_surf=N_bandage)
+skin_mesh = softbody.add_thinshell(pv.Cube(center=(0, 0, 0), x_length=0.02, y_length=0.02, z_length=0.002), n_surf=N_skin)
+bandage_mesh = softbody.add_thinshell(pv.Cube(center=(0, 0, 0.002), x_length=0.02, y_length=0.02, z_length=0.002), n_surf=N_bandage)
 softbody.init_states()
 softbody.init_dist_constraints()
-softbody.add_multi_boundary_constrain(1, 0, 0.001, range(198, 296))
+softbody.init_shape_constraints_thinshell([1])
+softbody.add_multi_boundary_constrain(1, 0, 0.0025, range(200))
 softbody.fix_less_than(0, 0, 2)
-softbody.fix_point(1, 306)
-
-# %%
-softbody.V[400 + 306]
-
-# %%
-# color = np.zeros(400)
-# color[306] = 1
+softbody.fix_point(1, 50)
+# softbody.fix_less_than(1, 0.002, 2)
 
 # %%
 # softbody.fix_point(1, 1)
@@ -64,7 +56,7 @@ cloth_dist_stiffness = 1
 V_boundary_stiffness_ref = 0.1
 V_dist_stiffness = torch.ones_like(softbody.V_mass).to(cfg.device) * cloth_dist_stiffness
 V_boundary_stiffness = torch.ones((softbody.C_boundary_list[0].shape[0], 1)).to(cfg.device) * V_boundary_stiffness_ref
-V_shape_stiffness = torch.ones_like(softbody.V_mass).to(cfg.device)*3
+V_shape_stiffness = torch.ones_like(softbody.V_mass).to(cfg.device)*0.0005
 
 # %%
 filename = 'tissue2tissue_stiffness_' + str(V_boundary_stiffness_ref) + '.gif'
@@ -79,16 +71,11 @@ pl.add_text(stiffness_text)
 # pl.add_mesh(bandage_mesh, scalars=color, show_edges=True, edge_color='#b37164ff',  lighting=False,style='surface', label='Bandage')
 pl.add_legend()
 pl.camera_position = 'iso'
-pl.show()
-print(pl.camera_position)
 
 # %%
-softbody.V[306]
-
-# %%
-pl.open_gif(filename)
-for i in range(control_trajectory.shape[0]):
-    softbody.V[softbody.offset_list[1] + 306] = control_trajectory[i]
+# pl.open_gif(filename)
+for i in trange(control_trajectory.shape[0]):
+    softbody.V[softbody.offset_list[1] + 50] = control_trajectory[i]
     step_ref = XPBDStep(softbody,
                 V_dist_stiffness=V_dist_stiffness, 
                 V_shape_stiffness=V_shape_stiffness,
@@ -107,13 +94,14 @@ for i in range(control_trajectory.shape[0]):
     energy = get_energy_boundary(softbody, softbody.V, V_boundary_stiffness)
     # V_boundary_stiffness[energy.squeeze() > 0.5] = 1e-8
 
-    V_boundary_stiffness = V_boundary_stiffness * torch.sigmoid(5e9 * (1e-8 - energy)) + 1e-8 * torch.sigmoid(5e9 * (energy - 1e-8))
-    # print(energy)
+    V_boundary_stiffness = V_boundary_stiffness * torch.sigmoid(2e9 * (1e-8 - energy)) + 1e-8 * torch.sigmoid(1e9 * (energy - 1e-8))
+    # print(1e-8 * torch.sigmoid(1e9 * (energy - 1e-8)))
     skin_mesh.points = softbody.V[:N_skin].cpu().numpy()
     bandage_mesh.points = softbody.V[N_skin:N_bandage+N_skin].cpu().numpy()
     # pl.remove_actor(bandage_actor)
     # bandage_actor = pl.add_points(softbody.V[N_skin:N_skin+N_bandage].cpu().numpy(), color='r')
-    pl.write_frame()
-pl.close()
+    # pl.write_frame()
+    pl.show(interactive_update=True)
+# pl.close()
 
 
